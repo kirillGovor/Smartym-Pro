115f196803e1b8815c470b204c97f6c5332793a8
Kirill Govor <kirillll13562@gmail.com>
2020-02-08 15:39:11 +0300

    header was styled

--- smatriym_pro_test/src/components/header/header.tsx
+++ smatriym_pro_test/src/components/header/header.tsx
@@ -4,21 +4,21 @@ import "./header.css";
 const Header = () => {
   return (
     <header>
-      <div>
+      <div className="userHeaderList">
         <p>Users List</p>
         <span>874 users</span>s
       </div>
       <div>
-        <input type="text" placeholder="search"></input>
+        <input className="search" type="text" placeholder="search"></input>
       </div>
 
       <div>
-        <button>TAble FILTER</button>
-        <select>
-          <option>Электроник</option>
-          <option>Сыроежкин</option>
-          <option>Чижиков</option>
-          <option>Кукушкина</option>
+        <button className="tableFilterButton">TABLE FILTER</button>
+        <select className="headerOption">
+          <option>Dale McCormick</option>
+          <option>filter 2</option>
+          <option>filter 3 </option>
+          <option>filter 4</option>
         </select>
       </div>
     </header>
